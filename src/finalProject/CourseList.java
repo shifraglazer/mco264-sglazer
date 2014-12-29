973e01b556b6ae11ed28c144cc4200d1d27ac43f
shifraglazer <shifraglazer@gmail.com>
2014-12-29 13:20:29 -0800

    course binary search

--- src/finalProject/CourseList.java
+++ src/finalProject/CourseList.java
@@ -12,6 +12,7 @@ public CourseList(){
 public void addCourse(Course c) throws DuplicateDataException{
 	if(!courses.contains(c)){
 		courses.add(c);
+		Collections.sort(courses);
 	}
 	else{
 		throw new DuplicateDataException();
@@ -21,38 +22,38 @@ public void addCourse(String courseID, String courseName, Integer numCredits, St
 	Course course=new Course(courseID,courseName,numCredits,deptID);
 	if(!courses.contains(course)){
 		courses.add(course);
+		Collections.sort(courses);
 	}
 	else{
 		throw new DuplicateDataException();
 	}
 }
-public void modifyCourseName(String courseID,String courseName){
-	int index=courses.find(courseID);
+public void modifyCourseName(String courseID,String courseName)throws InvalidDataException{
+	int index=find(courseID);
 	courses.get(index).setCourseName(courseName);
 }
-public void modifyNumCredits(String courseID, int numCredits){
-	int index=courses.find(courseID);
+public void modifyNumCredits(String courseID, int numCredits)throws InvalidDataException{
+	int index=find(courseID);
 	courses.get(index).setNumberOfCredits(numCredits);
 }
-public void setDeptID(String courseID,String deptID){
-int index=courses.find(courseID);
+public void setDeptID(String courseID,String deptID)throws InvalidDataException{
+int index=find(courseID);
 courses.get(index).setDepartmentID(deptID);
 }
-public Course getCourse(String courseID){
-	int index=courses.find(courseID);
+public Course getCourse(String courseID)throws InvalidDataException{
+	int index=find(courseID);
 	return courses.get(index);
 }
-public String getCourseName(String courseID){
-	int index=courses.find(courseID);
+public String getCourseName(String courseID) throws InvalidDataException{
+	int index=find(courseID);
 	return courses.get(index).getCourseName();
 }
-public int getNumCredits(String courseID){
-	int index=courses.find(courseID);
+public int getNumCredits(String courseID) throws InvalidDataException{
+	int index=find(courseID);
 	return courses.get(index).getnumCredits();
 }
-public int find(String courseID){
-	
-	Collections.sort(courses);
-	return Collections.binarySearch(courses, courseID);
+public int find(String courseID) throws InvalidDataException{
+	Course course=new Course(courseID,null,3,null);
+	return Collections.binarySearch(courses, course);
 }
 }
